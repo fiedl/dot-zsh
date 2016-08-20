7c5ce814f7e0ecb4da7b6f8facd0e407e29b4bb8
Sebastian Fiedlschuster <sebastian@fiedlschuster.de>
2016-08-20 19:56:54 +0200

    Updating zsh configuration at Sa 20. Aug 19:56:54 CEST 2016.

--- README.md
+++ README.md
@@ -201,6 +201,29 @@ and press the up key.
 See also: https://coderwall.com/p/jpj_6q/zsh-better-history-searching-with-arrow-keys
 
 
+#### Home and end keys
+
+This makes them also work in the terminal.
+
+key[Home]=${terminfo[khome]}
+key[End]=${terminfo[kend]}
+key[Insert]=${terminfo[kich1]}
+key[Delete]=${terminfo[kdch1]}
+key[Up]=${terminfo[kcuu1]}
+key[Down]=${terminfo[kcud1]}
+key[Left]=${terminfo[kcub1]}
+key[Right]=${terminfo[kcuf1]}
+key[PageUp]=${terminfo[kpp]}
+key[PageDown]=${terminfo[knp]}
+[[ -n "${key[Home]}"    ]]  && bindkey  "${key[Home]}"    beginning-of-line
+[[ -n "${key[End]}"     ]]  && bindkey  "${key[End]}"     end-of-line
+[[ -n "${key[Insert]}"  ]]  && bindkey  "${key[Insert]}"  overwrite-mode
+[[ -n "${key[Delete]}"  ]]  && bindkey  "${key[Delete]}"  delete-char
+[[ -n "${key[Up]}"      ]]  && bindkey  "${key[Up]}"      up-line-or-beginning-search
+[[ -n "${key[Down]}"    ]]  && bindkey  "${key[Down]}"    down-line-or-beginning-search
+[[ -n "${key[Left]}"    ]]  && bindkey  "${key[Left]}"    backward-char
+[[ -n "${key[Right]}"   ]]  && bindkey  "${key[Right]}"   forward-char
+
 #### Command highlighting
 
 When you type, the zsh checks whether the command in known. If so,
--- oh-my-zsh-customization/plugins/highlight/highlight.plugin.zsh
+++ oh-my-zsh-customization/plugins/highlight/highlight.plugin.zsh
@@ -8,10 +8,37 @@
 #
 # See also: https://coderwall.com/p/jpj_6q/zsh-better-history-searching-with-arrow-keys
 #
-autoload -U up-line-or-beginning-search
-autoload -U down-line-or-beginning-search
+bindkey -e
+typeset -A key
+autoload -Uz up-line-or-beginning-search
+autoload -Uz down-line-or-beginning-search
 zle -N up-line-or-beginning-search
 zle -N down-line-or-beginning-search
+bindkey "^[[A" up-line-or-beginning-search # Up
+bindkey "^[[B" down-line-or-beginning-search # Down
+
+# ## Home and end keys
+#
+# This makes them also work in the terminal.
+#
+# key[Home]=${terminfo[khome]}
+# key[End]=${terminfo[kend]}
+# key[Insert]=${terminfo[kich1]}
+# key[Delete]=${terminfo[kdch1]}
+# key[Up]=${terminfo[kcuu1]}
+# key[Down]=${terminfo[kcud1]}
+# key[Left]=${terminfo[kcub1]}
+# key[Right]=${terminfo[kcuf1]}
+# key[PageUp]=${terminfo[kpp]}
+# key[PageDown]=${terminfo[knp]}
+# [[ -n "${key[Home]}"    ]]  && bindkey  "${key[Home]}"    beginning-of-line
+# [[ -n "${key[End]}"     ]]  && bindkey  "${key[End]}"     end-of-line
+# [[ -n "${key[Insert]}"  ]]  && bindkey  "${key[Insert]}"  overwrite-mode
+# [[ -n "${key[Delete]}"  ]]  && bindkey  "${key[Delete]}"  delete-char
+# [[ -n "${key[Up]}"      ]]  && bindkey  "${key[Up]}"      up-line-or-beginning-search
+# [[ -n "${key[Down]}"    ]]  && bindkey  "${key[Down]}"    down-line-or-beginning-search
+# [[ -n "${key[Left]}"    ]]  && bindkey  "${key[Left]}"    backward-char
+# [[ -n "${key[Right]}"   ]]  && bindkey  "${key[Right]}"   forward-char
 
 # ## Command highlighting
 #
@@ -23,4 +50,5 @@ zle -N down-line-or-beginning-search
 # and got the idea from
 # https://github.com/idk/moo-skel.
 #
+ZSH_HIGHLIGHT_HIGHLIGHTERS=(main brackets pattern cursor)
 source "$ZSH_CUSTOM/plugins/highlight/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"
